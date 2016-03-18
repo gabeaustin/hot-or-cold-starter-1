56c75ed85d1eb761b2187aad1ffcd8612cb5d67e
Gabe Austin <gabriel.austin1@gmail.com>
2016-03-18 07:38:46 -0500

    added game functionality

--- js/app.js
+++ js/app.js
@@ -13,17 +13,57 @@ $(document).ready(function(){
   	});
 
     /*--- reloads the page when New Game button is clicked ---*/
-    function newGame() {
-      $(document).on("click", ".new", function() {
-        location.reload(true);
+    // function newGame() {
+      // $(document).on("click", ".new", function() {
+        // location.reload(true);
 
         // reset everything back to the beginning state
         // dont use location
-      });
-    };
+      // });
+    // };
 
-    newGame();
+    // newGame();
 
+    // random number 1 - 100
+    function getRandomNumber() {
+      return Math.floor(Math.random() * 100) + 1;
+    }
+
+    // store the random number
+    var randomNumber = getRandomNumber();
+    var count = 0;
+    $('#guessButton').click(function() {
+      var userGuess = $('#userGuess').val();
+    
+
+    // feedback to user
+    var result = checkNumber(randomNumber, userGuess);
+    $('#feedback').html(result);
+
+    // put the guessed numbers in a list
+    $('ul#guessList').append('<li>' + userGuess + '</li>');
+
+    });
+
+    // check to see how close a user is to the random number
+    function checkNumber (randomNumber, userGuess) {
+      var diff = Math.abs(randomNumber - userGuess);
+      if (diff > 50){
+        return "You are freezing cold";
+      } else if (diff > 30) {
+        return "Cold";
+      } else if (diff > 20) {
+        return "Warm";
+      } else if (diff > 10) {
+        return "Hot";
+      } else if (diff >= 1) {
+        return "You are burning up!!!";
+      } else if (diff === 0) {
+        return "Congrats!! You've guessed the right number!";
+      } else {
+        return "Keep guessing...";
+      }
+    }
 
 });
 
